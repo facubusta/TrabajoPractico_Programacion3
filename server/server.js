437d81c4f62e7ff679388a8231e41f8dd1d90129
Federico <federico.m.robledo@gmail.com>
2025-10-24 20:19:07 -0300

    Primera ruta de la api para listar productos, cors incluido, traer producto por id, filtros y paginacion

--- server/server.js
+++ server/server.js
@@ -1,23 +1,35 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import productsApi from "./routes/api.products.js";
+
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(cors({
+  origin: true,           
+  credentials: false
+}));
+
 const PORT = process.env.PORT || 3000;
 
-// Servir contenido estático del frontend, lo tomamos desde la carpeta public, carga por defecto index.html
+app.use(express.json()); // para parsear JSON en el body
+
+// API de productos
+app.use("/api/products", productsApi);
+
+// archivos estáticos del frontend
 app.use(express.static(path.join(__dirname, "../frontend/public")));
 
-// Si no le pega a una ruta válida por ahora devolvemos igual index.html para 
-// que el frontend maneje el enrutamiento de a que pantalla llevarlo, por ejemplo una de 404.
+// catch-all (para redirigir rutas no manejadas al index.html del frontend)
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
 });
 
-// Server escuchando en el puerto definido
 app.listen(PORT, () => {
   console.log(`Servidor levantado en http://localhost:${PORT}`);
 });
